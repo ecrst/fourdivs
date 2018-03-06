68acb35358f7b6d19878b86b49dc768c6b3f13b7
Eugene Crystal <ugncry@gmail.com>
2018-03-06 13:53:57 +0300

    removed wrappers; added resizers

--- script.js
+++ script.js
@@ -2,101 +2,48 @@
   'use strict';
 }()); //USE strict
 
-const props = (function() {
-  let boxes = document.getElementsByClassName('wrapper-box');
-  return {
-    boxes: {
-      box1: boxes[0],
-      box2: boxes[1],
-      box3: boxes[2],
-      box4: boxes[3],
-    },
-    borderWidth: parseInt(getComputedStyle(boxes[0])
-    .getPropertyValue('border-right-width'), 10) * 2,
+const boxes = (function() {
+  let boxesArray = document.getElementsByClassName('content-box');
+  let boxes = {}, i = 1;
+  for (box of boxesArray) {
+    boxes['box' + i++] = box;
   }
+  return boxes;
 }());
 
+for (resizer of document.getElementsByClassName('resizer')) {
 
-document.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  if (!isBorder(e.target)) return;
-  let currentCoords = {
-    x: e.x,
-    y: e.y,
-  }
-  let boxes = getBoxes(currentCoords.x, currentCoords.y, props.borderWidth);
-  if (!boxes) return;
-
-  document.addEventListener('mousemove', resize)
-  document.addEventListener('mouseup', (e) => {
-    document.removeEventListener('mousemove', resize)
-  })
-
-  function resize(e) {
-    return boxes.hasOwnProperty('box1') ? resizeVert(e) : resizeHor(e)
-  }
-  function resizeVert(e) {
-    console.log('kek');
-    let change = currentCoords.x - e.x;
-    currentCoords.x = e.x;
-    boxes.box2.style.width = (parseInt(getComputedStyle(boxes.box2, '').width) + change) + 'px';
-    boxes.box4.style.width = (parseInt(getComputedStyle(boxes.box4, '').width) + change) + 'px';
-    boxes.box1.style.width = (parseInt(getComputedStyle(boxes.box1, '').width) - change) + 'px';
-    boxes.box3.style.width = (parseInt(getComputedStyle(boxes.box3, '').width) - change) + 'px';
-  }
-  function resizeHor(e) {
-    console.log('keks');
-    let change = currentCoords.y - e.y;
-    currentCoords.y = e.y;
-    boxes.top.style.height = (parseInt(getComputedStyle(boxes.top, '').height) - change) + 'px';
-    boxes.bot.style.height = (parseInt(getComputedStyle(boxes.bot, '').height) + change) + 'px';
-  }
-
-  function isBorder(el) {
-    return el.classList.contains('wrapper-box')
-  }
-})
-
-/**
-* Is clicked element a border
-* @param {Object} el - The element.
-* @returns {boolean} - True if object is a wrapper (border) or false if it's not.
-*/
-
-
-/**
-* Get paricipating boxes which are involved to resize
-* @param {integer} x - X coordinates
-* @param {integer} y - Y coordinates
-* @param {integer} borderWidth - The width of box's border
-* @returns {null||Object||Object}
-*/
-const getBoxes = (x, y, borderWidth) => {
-  // [top, low, left, right] elements.
-  let els = [[x, y - borderWidth], [x, y + borderWidth],
-            [x - borderWidth, y], [x + borderWidth, y]]
-            .map(convertCoordinatesToElemets)
-
-  if (els.every(b => b == null || b.classList.contains('wrapper-box'))) return null;
-  console.log(els);
-  return (isNullOrWrapper(els[0]) && isNullOrWrapper(els[1])) ? props.boxes :
-           {
-             top: els[0].parentElement,
-             bot: els[1].parentElement,
-           }
-
-
-  function isNullOrWrapper(el) {
-    return el === null || el.classList.contains('wrapper-box')
-  }
-  function convertCoordinatesToElemets(c) {
-    let el = document.elementFromPoint(c[0], c[1]);
-    if (el === null) {
-      return null;
-    } else if (el.classList.contains('header')) {
-      return el.parentElement;
+  resizer.addEventListener('mousedown', e => {
+    e.preventDefault();
+    let resizer = e.target
+    if (resizer.classList.contains('vertical')) {
+      document.body.style.cursor = 'e-resize';
     } else {
-      return el;
+      document.body.style.cursor = 'n-resize';
     }
-  }
+
+    document.addEventListener('mousemove', resize);
+    document.addEventListener('mouseup', e => {
+      document.removeEventListener('mousemove', resize);
+      document.body.style.cursor = 'auto';
+    });
+
+    function resize(e) {
+      return resizer.classList.contains('vertical') ? resizeX(e) : resizeY(e)
+      e.preventDefault();
+    }
+    function resizeX(e) {
+      let bodyWidth = 100 / document.body.clientWidth;
+      let leftHeight = (parseFloat(getComputedStyle(boxes.box1, '').width) + e.movementX) * bodyWidth;
+      boxes.box1.style.width =  leftHeight + '%';
+      boxes.box2.style.width =  (100 - leftHeight) + '%';
+      boxes.box3.style.width =  (100 - leftHeight) + '%';
+    }
+    function resizeY(e) {
+      let bodyHeight = 100 / document.body.clientHeight;
+      let topHeight = (parseFloat(getComputedStyle(boxes.box2, '').height) + e.movementY) * bodyHeight;
+      boxes.box2.style.height = topHeight + '%';
+      boxes.box3.style.height = (100 - topHeight) + '%';
+    }
+  })
 }
